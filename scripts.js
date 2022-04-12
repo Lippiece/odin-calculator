b4c57f30b13bf7176a9b0988a5bac0f888eb7a15
Lippiece <seriouss7222@gmail.com>
2022-04-12 12:47:52 +0200

    Add the +,-,/,* functions

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let add = (input1, input2) => {
+	return input1 + input2;
+};
+let substract = (input1, input2) => {
+	return input1 - input2;
+};
+let multiply = (input1, input2) => {
+	return input1 * input2;
+};
+let divide = (input1, input2) => {
+	if (input2 !== 0) {
+		return input1 / input2;
+	}
+};
