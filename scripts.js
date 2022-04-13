be9503567b740edc4932eae7b7cd98f9c22271e4
Lippiece <seriouss7222@gmail.com>
2022-04-13 13:50:53 +0200

    Write the functions with numbers

--- scripts.js
+++ scripts.js
@@ -2,11 +2,20 @@ let inputs;
 let operators;
 let output;
 
-let add = (input1, input2) => {
-	return input1 + input2;
+let add = (inputs1, inputs2) => {
+	return parseInt(inputs1) + parseInt(inputs2);
 };
-let substract = (input1, input2) => {
-	return input1 - input2;
+let substract = (inputs1, inputs2) => {
+	return parseInt(inputs1) - parseInt(inputs2);
+};
+let multiply = (inputs1, inputs2) => {
+	return parseInt(inputs1) * parseInt(inputs2);
+};
+let divide = (inputs1, inputs2) => {
+	if (parseInt(inputs2) !== 0) {
+		return parseInt(inputs1) / parseInt(inputs2);
+	}
+};
 };
 let multiply = (input1, input2) => {
 	return input1 * input2;
