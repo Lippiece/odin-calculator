47e9b98da93ed3ebd365d02c611299843abac8c3
Lippiece <seriouss7222@gmail.com>
2022-04-13 13:52:07 +0200

    Write the input parser mockup

--- scripts.js
+++ scripts.js
@@ -16,6 +16,16 @@ let divide = (inputs1, inputs2) => {
 		return parseInt(inputs1) / parseInt(inputs2);
 	}
 };
+
+let getInput = () => {
+	let input = "44 + 55 / 66";
+	/* Put the inputs and operators in arrays */
+	inputs = input.match(/\d+/g);
+	inputs = inputs.map(x => parseInt(x));
+	operators = input.match(/ \D/g);
+	/* Remove the whitespaces */
+	operators = operators.map(x => x.replace(/\s/, ""));
+	console.log("Inputs and operators: ",inputs, operators);
 };
 let multiply = (input1, input2) => {
 	return input1 * input2;
