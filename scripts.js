a7e321de0c948a63c0efaf4f8c402fcbf2f9b872
Lippiece <seriouss7222@gmail.com>
2022-04-14 13:34:43 +0200

    Change the arrow functions to functions

--- scripts.js
+++ scripts.js
@@ -6,33 +6,31 @@ let operands = [];
 let operators = [];
 let pressed;
 
-let add = (inputs1, inputs2) => {
+function add(inputs1, inputs2) {
 	return parseFloat(inputs1) + parseFloat(inputs2);
-};
-let substract = (inputs1, inputs2) => {
+}
+function substract(inputs1, inputs2) {
 	return parseFloat(inputs1) - parseFloat(inputs2);
-};
-let multiply = (inputs1, inputs2) => {
+}
+function multiply(inputs1, inputs2) {
 	return parseFloat(inputs1) * parseFloat(inputs2);
-};
-let divide = (inputs1, inputs2) => {
+}
+function divide(inputs1, inputs2) {
 	if (parseFloat(inputs2) !== 0) {
 		return parseFloat(inputs1) / parseFloat(inputs2);
 	}
-};
+}
 
-let getInput = () => {
-	let input = "44 + 55 / 66";
+function getInput() {
 	/* Put the inputs and operators in arrays */
 	inputs = input.match(/\d+/g);
 	inputs = inputs.map(x => parseFloat(x));
 	operators = input.match(/ \D/g);
 	/* Remove the whitespaces */
 	operators = operators.map(x => x.replace(/\s/, ""));
-	console.log("Inputs and operators: ",inputs, operators);
-};
+}
 
-let evaluate = () => {
+function evaluate() {
 	for (let i = 0; i < operators.length; i++) {
 		switch (operators[i]) {
 		case "+":
@@ -52,7 +50,7 @@ let evaluate = () => {
 			break;
 		}
 	}
-};
+}
 
 for (let i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", () => {
