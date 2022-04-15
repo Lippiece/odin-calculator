a3f1e19903bed0c512760fb0685c6c014b5dafb6
Lippiece <seriouss7222@gmail.com>
2022-04-15 16:26:04 +0200

    feat: Add decimalAdjust and round10 function
    They can be merged, initially existed as multiple variations of rounding

--- scripts.js
+++ scripts.js
@@ -39,6 +39,29 @@ function getInput() {
 	console.log("Operands and operators: ", operands, operators);
 }
 
+/* Stolen from W3C */
+function decimalAdjust(type, value, exp) {
+	// If the exp is undefined or zero...
+	if (typeof exp === "undefined" || +exp === 0) {
+		return Math[type](value);
+	}
+	value = +value;
+	exp = +exp;
+	// If the value is not a number or the exp is not an integer...
+	if (isNaN(value) || !(typeof exp === "number" && exp % 1 === 0)) {
+		return NaN;
+	}
+	// Shift
+	value = value.toString().split("e");
+	value = Math[type](+(value[0] + "e" + (value[1] ? (+value[1] - exp) : -exp)));
+	// Shift back
+	value = value.toString().split("e");
+	return +(value[0] + "e" + (value[1] ? (+value[1] + exp) : exp));
+}
+function round10(value, exp) {
+	return decimalAdjust("round", value, exp);
+}
+
 /* Perform the operations and trim the output array */
 function evaluate() {
 	for (let i = 0; i < operators.length; i++) {
